255ad752893fc162a207079b314480fa8cfacdb7
soundsnick <soundsnick@gmail.com>
2019-02-05 19:09:52 +0600

    sdadsd

--- mail.php
+++ mail.php
@@ -10,7 +10,7 @@
     $headers .= "From: Mishki Roze <support@rosemary.kz>\r\n";
     $headers .= "Bcc: support@rosemary.kz\r\n";
     // mail($to, $subject, $message, $headers);
-    mail("soundsnick@gmail.com", $subject, $message,
+    mail("rosemary_gifts@gmail.com", $subject, $message,
      "From: support@rosemary.kz\r\n"
     ."Reply-To: support@rosemary.kz\r\n");
     echo 'Спасибо. Мы с вами свяжемся';
